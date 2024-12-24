1b7543e791f3deb91bf3a1f5d0764cbebb9f7aac
yooguitar <kitae1996@gmail.com>
2024-12-24 11:43:06 +0900

    Member VO 생성
    Member, NonUser, BusinessUser VO

---
+++ AllThatTip/src/main/java/com/kh/AllThatTrip/member/model/vo/NonUser.java
@@ -0,0 +1,24 @@
+package com.kh.AllThatTrip.member.model.vo;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class NonUser {
+
+	private Long rsvNo;
+	private String nonUserName;
+	private String phone;
+	private Date rsvDate;
+	
+	
+}
