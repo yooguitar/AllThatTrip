c9a246b5371d76920f5a3c7bf2d40600ce8e81fe
SookjinNamgung <rwsj051106@gmail.com>
2024-12-24 15:05:54 +0900

    오류수정했어요! 업데이트해주세요!

--- AllThatTip/src/main/java/com/kh/AllThatTrip/board/controller/BoardController.java
+++ AllThatTip/src/main/java/com/kh/AllThatTrip/board/controller/BoardController.java
@@ -2,12 +2,17 @@ package com.kh.AllThatTrip.board.controller;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.AllThatTrip.board.model.service.BoardService;
+import com.kh.AllThatTrip.board.model.vo.Board;
 import com.kh.AllThatTrip.common.ModelAndViewUtil;
 
 import lombok.RequiredArgsConstructor;
@@ -15,29 +20,39 @@ import lombok.extern.slf4j.Slf4j;
 
 
 @Controller
-@RequiredArgsConstructor // 생성자 주입으로하는게 좋음!롬복이 알아서 해준다
+@RequiredArgsConstructor
 @Slf4j
 public class BoardController {
+	/*
 
 	private final BoardService boardService;
 	private final ModelAndViewUtil mv;
 	
 	// 페이징바
 	@GetMapping("boards")
-	public ModelAndView selectBoardList(@RequestParam(value="page", defaultValue="1") int page) {
+	public ModelAndView selectBoardList(@RequestParam(value="page", defaultValue="1") int currentPage) {
 		
-		Map<String, Object> map = boardService.selectBoardList(page);
+		Map<String, Object> map = boardService.selectBoardList(currentPage);
 
 		return mv.setViewNameAndData("board/list", map);
 	}
 	
 	// 전체 리스트 조회
 	
+	
 	// 조회수 증가
 	
 	// 상세 조회
 	
 	// 등록
+	@PostMapping("boards")
+	public ModelAndView insertBoard(Board board, MultipartFile upfile, HttpSession session) {
+		
+		boardService.insertBoard(board, upfile);
+		session.setAttribute("alertMsg", "게시글 등록 성공");
+		return mv.setViewNameAndData("redirect:boards", null);
+		
+	}
 	
 	// 수정
 	
@@ -52,7 +67,7 @@ public class BoardController {
 	// 댓글ㄴ
 	
 	
-	
+	*/
 	
 	
 }
