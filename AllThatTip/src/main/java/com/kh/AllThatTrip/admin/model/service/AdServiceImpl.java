ce1085b0ed041b65697d75b984ec45f8fb17d35c
JiJohnYuno <gng0202@naver.com>
2024-12-26 10:05:56 +0900

    10:05 first commit
    지존멋짐 ㅋㅋ

--- AllThatTip/src/main/java/com/kh/AllThatTrip/admin/model/service/AdServiceImpl.java
+++ AllThatTip/src/main/java/com/kh/AllThatTrip/admin/model/service/AdServiceImpl.java
@@ -2,4 +2,15 @@ package com.kh.AllThatTrip.admin.model.service;
 
 public class AdServiceImpl {
 
+	private final AdminMapper mapper;
+
+
+	private int getTotalCount() {
+		int totalCount = mapper.selectTotalCount();
+
+		if(totalCount == 0) {
+			throw new BoardNotFoundException("게시글이 없습니다");
+		}
+		return totalCount;
+	}
 }
